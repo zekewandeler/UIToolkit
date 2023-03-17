cada2b7f9ce0412b0e0d6d9b6fcea8ba6ec93a0e
Zeke Wandeler <77249187+zekewandeler@users.noreply.github.com>
2023-03-16 23:48:44 -0600

    Initial commit

---
+++ Assets/InventorySlot.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+//using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class InventorySlot : VisualElement
+{
+    public Image Icon;
+    public string ItemGuid = "";
+    public InventorySlot()
+    {
+        //Create a new Image element and add it to the root
+        Icon = new Image();
+        Add(Icon);
+        //Add USS style properties to the elements
+        Icon.AddToClassList("slotIcon");
+        AddToClassList("slotContainer");
+    }
+}
